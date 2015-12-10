365a5e597de5337cebf75132dd7b9ba97ebe6ef9
sbaldawa <sbaldawa@users.noreply.github.com>
2015-12-10 12:07:12 +1100

    Create plot1.R
    Course Project 1

---
+++ plot1.R
@@ -0,0 +1,5 @@
+data <- read.table("H:/R/Exploratory Data Analysis/household_power_consumption.txt",header = T,sep=";")
+newdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+gap <- as.numeric(newdata$Global_active_power)
+png("plot1.png",width=480,height=480)
+hist(gap,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
