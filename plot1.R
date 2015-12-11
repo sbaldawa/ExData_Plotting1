62c460e59db597636879f3f56eda03223d144f20
sbaldawa <sbaldawa@users.noreply.github.com>
2015-12-11 12:55:34 +1100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,3 +3,4 @@ newdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
 gap <- as.numeric(newdata$Global_active_power)
 png("plot1.png",width=480,height=480)
 hist(gap,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+dev.off()
